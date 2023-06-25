46271dd15ffd9a73acf3d845184779f097ee3244
Weronika2003 <creative.name2003@gmail.com>
2023-06-25 13:51:19 +0200

    1

--- src/01-color-switcher.js
+++ src/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+ 
+let time = null;
+stopButton.setAttribute('disabled', true);
+
+startButton.addEventListener('click', () => {
+  stopButton.removeAttribute('disabled');
+  startButton.setAttribute('disabled', true);
+  time = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', true);
+  clearInterval(time);
+})
---
+++ src/02-timer.js
@@ -0,0 +1,2 @@
+import flatpickr from 'flatpickr';
+import Notiflix from 'notiflix';
