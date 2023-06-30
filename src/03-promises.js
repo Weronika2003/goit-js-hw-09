a1af2e8e6f1411238bc78ae6f08fd9f2d19c57cd
Weronika2003 <creative.name2003@gmail.com>
2023-06-30 16:23:51 +0200

    Update 03-promises.js

--- src/03-promises.js
+++ src/03-promises.js
@@ -1,3 +1,9 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delayInput = document.querySelector("input[name='delay']");
+const stepInput = document.querySelector("input[name='step']");
+const amountInput = document.querySelector("input[name='amount']")
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +12,10 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
